3a91fcc1d67bd1978f63edde32ae9d6322c1d0bd
Farhan Julana <46250914+farhanjln@users.noreply.github.com>
2020-11-01 17:36:45 +0700

    Add files via upload

---
+++ Web_Scrapping_Test.py
@@ -0,0 +1,16 @@
+
+import selenium
+import matplotlib as plt
+f (int[5,7,8,3]) returns int
+    int i = 0
+    int s = 0
+    while i < length (f)
+    s = s + f[i]
+    i = i + 1
+    end
+return s
+end
+
+
+
+
