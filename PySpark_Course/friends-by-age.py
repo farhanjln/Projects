3e351062be52ba6b9a188dfb52529fc038aa49c6
Farhan Julana <46250914+farhanjln@users.noreply.github.com>
2020-11-03 20:10:56 +0700

    Add files via upload

---
+++ PySpark_Course/friends-by-age.py
@@ -0,0 +1,21 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("Friends_by_Age")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
+
+
+lines =  sc.textFile("file:///C:/Users/---/Desktop/Projects/Practice/Spark_Course/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+results = averagesByAge.collect()
+results.round(2)
+for result in results:
+    print(result)
+    
---
+++ PySpark_Course/ratings-counter.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+## you can change the directory depend on the file location of the RDD object
+lines = sc.textFile("file:///C:/Users/---/Desktop/Projects/Practice/Spark_Course/ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
